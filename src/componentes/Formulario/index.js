1608ddd5467a7509b1b89802c7a5fb611119fca1
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-26 19:13:17 -0300

    Second class

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
-import  CampoTexto from '../CampoTexto/CampoTexto.js'
+import Botao from '../Botao/index.js'
+import CampoTexto from '../CampoTexto/CampoTexto.js'
+import ListaSuspensa from '../ListaSuspensa/index.js'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        ' Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Foi submetido');
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+                <Botao texto="Criar Card"/>
             </form>
         </section>
     )
