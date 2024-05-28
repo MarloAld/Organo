aaaa24df30bb916a24757b6b31b0a4e54a4f84ab
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-28 13:01:41 -0300

    Third class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/banner';
 import Formulario from './componentes/Formulario';
+import Time from './Time';
 
 function App() {
+  
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner /> 
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="FrontEnd"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
