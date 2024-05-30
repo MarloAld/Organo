4afdaf986ad737e823d442230f9ec9b081b3b8d1
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-30 11:23:05 -0300

    Fourth class

--- src/Time/index.js
+++ src/Time/index.js
@@ -1,11 +1,16 @@
+import Colaborador from '../componentes/Colaborador'
 import './Time.css'
 
 const Time = (props) => {
     return (
-        <section>
-            <h3>{props.nome}</h3>
+        props.colaboradores.length > 0 &&
+        <section className='time' style={{backgroundColor: props.corSecundaria}}>
+            <h3 style={{borderBottomColor: props.corPrimaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
-    )
+    ) // return condição 1 && termo a ser retornado caso condição 1 = true. Retorna o que está após o operador and | também pode ser usado o ternário: condição ? jsx : ''
 }
 
 export default Time
