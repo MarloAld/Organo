702456d2bb49b80f6fdb8176ad3a3319d4a31203
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-30 11:46:29 -0300

    Final version

--- src/Time/index.js
+++ src/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
         <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{borderBottomColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
             </div>
         </section>
     ) // return condição 1 && termo a ser retornado caso condição 1 = true. Retorna o que está após o operador and | também pode ser usado o ternário: condição ? jsx : ''
